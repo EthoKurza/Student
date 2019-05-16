3217e846a30add9b5d108728efa7139a97e5da10
EthoKurza <36766454+EthoKurza@users.noreply.github.com>
2019-05-16 17:39:24 -0400

    Setting Student IDs and Printing each student with the ID.

--- src/studentobject_example/StudentObject_Example.java
+++ src/studentobject_example/StudentObject_Example.java
@@ -33,10 +33,13 @@ public class StudentObject_Example {
         }*/
         Student s1 = new Student();
         s1.setName("Bob");
+        s1.setID(1);
         Student s2 = new Student();
         s2.setName("Cob");
+        s2.setID(2);
         Student s3 = new Student();
         s3.setName("Rob");
+        s3.setID(3);
         
         Student[] sList = new Student[3];//student array
         sList[0] = s1;
@@ -44,7 +47,7 @@ public class StudentObject_Example {
         sList[2] = s3;
         
         for(int i=0;i<sList.length;i++){
-            System.out.println(sList[i].getName());
+            System.out.println("Name: "+sList[i].getName()+"\nStudent ID: "+ sList[i].getID());
         }
         
     }//end of main
