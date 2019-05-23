795bd99f860a5cc68228a4a57d1ed96089ab45de
EthoKurza <36766454+EthoKurza@users.noreply.github.com>
2019-05-23 16:38:48 -0400

    Update StudentObject_Example.java

--- src/studentobject_example/StudentObject_Example.java
+++ src/studentobject_example/StudentObject_Example.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * Thursday, May 23,2019
+ * Thursday, May 23,2019 @Sheridan College
  */
 package studentobject_example;
 
